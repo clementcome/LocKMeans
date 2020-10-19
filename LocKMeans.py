1f3a5c16300219e19a20c91723f9bb0acb47100b
clement <clement.come@student-cs.fr>
2020-10-19 14:24:55 +0200

    Adding examples in notebook

--- LocKMeans.py
+++ LocKMeans.py
@@ -46,19 +46,26 @@ class LocKMeans:
             self.cluster_size_ = np.repeat(avg_cluster_size, self.n_clusters_)
         self.cluster_centers_ = initial_centers
 
+        copy_X = X.copy()
+        points_cluster_order = np.tile(
+            np.arange(self.n_clusters_).reshape((1, -1)), (n_samples, 1)
+        )
+
         for i in tqdm(range(self.max_iter_), disable=self.hide_pbar_):
-            self.labels_ = np.repeat(-1, X.shape[0])
             list_points_in_clusters = [[] for _ in range(self.n_clusters_)]
             list_cluster_size = [0 for _ in range(self.n_clusters_)]
-            dist_data_centers = cdist(X, self.cluster_centers_)
-            sort_index_data_centers = np.argsort(np.min(dist_data_centers, axis=1))
-            for idx in sort_index_data_centers:
-                cluster_order = np.argsort(dist_data_centers[idx])
+            dist_data_centers = cdist(copy_X, self.cluster_centers_)
+            sort_index_data_centers = np.argsort(
+                np.min(dist_data_centers, axis=1), kind="stable"
+            )
+            dist_data_centers = dist_data_centers[sort_index_data_centers]
+            copy_X = copy_X[sort_index_data_centers]
+            for idx, dist_data_center_1 in enumerate(dist_data_centers):
+                cluster_order = np.argsort(dist_data_center_1)
                 for cluster_idx in cluster_order:
                     if list_cluster_size[cluster_idx] < self.cluster_size_[cluster_idx]:
-                        list_points_in_clusters[cluster_idx].append(X[idx])
+                        list_points_in_clusters[cluster_idx].append(copy_X[idx])
                         list_cluster_size[cluster_idx] += 1
-                        self.labels_[idx] = cluster_idx
                         break
             new_centers = np.zeros_like(self.cluster_centers_)
             for cluster_idx in range(self.n_clusters_):
